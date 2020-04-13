266313594ff4c0ea3755a8f76d922534300b46ef
Vinit Shandilya <vinitshandilya@outlook.com>
2020-04-13 19:31:43 +0530

    Milestone bug fix!

---
+++ app/src/main/java/com/dragonfire/wildfirelauncher/AppActionDownListener.java
@@ -0,0 +1,7 @@
+package com.dragonfire.wildfirelauncher;
+
+import android.view.View;
+
+public interface AppActionDownListener {
+    void onAppActionDown(AppObject appObject, View clickedView);
+}
